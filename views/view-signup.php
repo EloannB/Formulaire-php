d9f3b2d0554c5d24408758a7780d631ca75bad9f
Eloann <eloann76@gmail.com>
2024-01-13 20:26:53 +0100

    ajout de nouveaux champs

--- views/view-signup.php
+++ views/view-signup.php
@@ -18,6 +18,8 @@
                 <p class="error"><?= $erreurs["nom"] ?? '' ?></p>
                 <input type="text" id="prenom" name="prenom" placeholder="Prénom" value="<?= htmlspecialchars($_POST['prenom'] ?? '') ?>" required>
                 <p class="error"><?= $erreurs["prénom"] ?? '' ?></p>
+                <input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" value="<?= htmlspecialchars($_POST['pseudo'] ?? '') ?>" required>
+                <p class="error"><?= $erreurs["pseudo"] ?? '' ?></p>
                 <input type="email" id="courriel" name="courriel" placeholder="Courriel" value="<?= htmlspecialchars($_POST['courriel'] ?? '') ?>" required>
                 <p class="error"><?= $erreurs["courriel"] ?? '' ?></p>
                 <input type="date" id="date_naissance" name="date_naissance" placeholder="Date de naissance" value="<?= htmlspecialchars($_POST['date_naissance'] ?? '') ?>" required>
@@ -26,6 +28,11 @@
                 <p class="error"><?= $erreurs["mot_de_passe"] ?? '' ?></p>
                 <input type="password" id="conf_mot_de_passe" name="conf_mot_de_passe" placeholder="Confirmer le mot de passe" required>
                 <p class="error"><?= $erreurs["conf_mot_de_passe"] ?? '' ?></p>
+                <select id="choix_entreprise" name="choix_entreprise" required>
+                    <option value="" disabled selected>Sélectionnez une entreprise</option>
+                    <option value="entreprise1" <?= isset($_POST['choix_entreprise']) && $_POST['choix_entreprise'] == 'entreprise1' ? 'selected' : '' ?>>Entreprise 1</option>
+                    <option value="entreprise2" <?= isset($_POST['choix_entreprise']) && $_POST['choix_entreprise'] == 'entreprise2' ? 'selected' : '' ?>>Entreprise 2</option>
+                </select>
                 <label>
                     <input type="checkbox" name="cgu" <?= isset($_POST['cgu']) ? 'checked' : '' ?> required>
                     J'accepte les conditions générales d'utilisation (CGU)
