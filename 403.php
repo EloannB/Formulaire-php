6d951b8336f9fd3220b30b5ad1fd33ea044f70b6
Eloann <eloann76@gmail.com>
2024-02-12 13:34:54 +0100

    ajout erreur 403, 404

---
+++ 403.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/403.css">
+    <title>Erreur 403</title>
+</head>
+
+<body>
+
+<div class="base io"> 
+  <h1 class="io">403</h1>
+  <h2>Accès Refusé</h2>
+  <h5>(Dommage mec...)</h5>
+</div>
+
+
+</body>
+
+</html>
---
+++ 404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/404.css">
+    <title>Erreur 404</title>
+</head>
+
+<body>
+
+    <div class="number">404</div>
+    <div class="text"><span>Ooops...</span><br>page introuvable</div>
+    <a class="me" href="https://codepen.io/uzcho_/pens/popular/?grid_type=list" target="_blank"></a>
+
+</body>
+
+</html>
